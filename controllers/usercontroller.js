aeb72cd293e44e24d6c44f3472790515e95184f0
Lauren Pineiro <lamor1800@gmail.com>
2022-11-03 19:48:09 -0400

    started adding user controllers

---
+++ controllers/usercontroller.js
@@ -0,0 +1,46 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+
+  // get all users
+  getAllUser(req, res) {
+    User.find({})
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // create User
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
